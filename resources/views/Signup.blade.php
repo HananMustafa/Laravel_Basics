cc5c7d215f9bc5904fb379ded87a285d0fc4b9a4
HananMustafa <hananmustafaawan@gmail.com>
2024-08-23 14:46:24 +0500

    Designed the Signup Form

--- resources/views/Signup.blade.php
+++ resources/views/Signup.blade.php
@@ -1,45 +1,50 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Signup Form</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h2>Signup Form</h2>
+    <div class="formbg">
+        <h2>Signup Form</h2>
 
-    @if (session('success'))
-        <div>{{ session('success') }}</div>
-    @endif
+        @if (session('success'))
+            <div>{{ session('success') }}</div>
+        @endif
 
-    <form action="{{ route('signup.process') }}" method="POST">
-        @csrf
-        <div>
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" value="{{ old('name') }}">
-            @error('name')
-                <div>{{ $message }}</div>
-            @enderror
-        </div>
-        <div>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" value="{{ old('email') }}">
-            @error('email')
-                <div>{{ $message }}</div>
-            @enderror
-        </div>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password">
-            @error('password')
-                <div>{{ $message }}</div>
-            @enderror
-        </div>
-        <div>
-            <label for="password_confirmation">Confirm Password:</label>
-            <input type="password" id="password_confirmation" name="password_confirmation">
-        </div>
-        <div>
-            <button type="submit">Signup</button>
-        </div>
-    </form>
+        <form action="{{ route('signup.process') }}" method="POST">
+            @csrf
+            <div class="field">
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" value="{{ old('name') }}">
+                @error('name')
+                    <div>{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="field">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" value="{{ old('email') }}">
+                @error('email')
+                    <div>{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="field">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password">
+                @error('password')
+                    <div>{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="field">
+                <label for="password_confirmation">Confirm Password:</label>
+                <input type="password" id="password_confirmation" name="password_confirmation">
+            </div>
+            <div>
+                <button type="submit" class="btn-submit">Signup</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
