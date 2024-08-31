c8df05e55e29a7b7d746d5f05adee1b576f19b32
HananMustafa <hananmustafaawan@gmail.com>
2024-08-31 12:34:31 +0500

    Grouping routes and passing to a middleware (to avoid putting same middleware on each route)

--- routes/web.php
+++ routes/web.php
@@ -8,36 +8,44 @@ use App\Http\Controllers\UpdateCustomerController;
 use App\Http\Middleware\ValidUser;
 
 
-//Welcome route
+//Welcome
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-//Signup routes
+//Signup
 Route::get('/signup', [SignupController::class, 'showSignupForm'])->name('signup.form');
 Route::post('/signup', [SignupController::class, 'processSignup'])->name('signup.process');
 
-//Login routes
+//Login
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.form');
 Route::post('/login', [LoginController::class, 'processLogin'])->name('login.process');
-
-// //Dashboard routes
-// Route::get('/dashboard', [LoginController::class, 'showDashboard'])->name('dashboard');
-
-//Add Customer form route
-Route::get('/add-customer', [AddCustomerController::class, 'showForm'])->name('add.customer.form')->middleware(ValidUser::class);
-Route::post('/add-customer', [AddCustomerController::class, 'storeCustomer'])->name('add.customer');
 
 //DASHBOARD
 Route::get('/dashboard', [LoginController::class, 'showDashboard'])->name('dashboard')->middleware(ValidUser::class);
 
-//Delete customer route
+
+//Add Customer
+Route::get('/add-customer', [AddCustomerController::class, 'showForm'])->name('add.customer.form')->middleware(ValidUser::class);
+Route::post('/add-customer', [AddCustomerController::class, 'storeCustomer'])->name('add.customer');
+
+//Delete Customer
 Route::delete('/customer/{id}', [AddCustomerController::class, 'destroy'])->name('delete.customer');
 
-//Edit Customer routes
+//Update Customer
 Route::get('/customer/{id}/edit', [UpdateCustomerController::class, 'edit'])->name('edit.customer')->middleware(ValidUser::class);
 Route::put('/customer/{id}', [UpdateCustomerController::class, 'update'])->name('update.customer');
 
-
 //Logout 
 Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+
+
+
+
+
+// Route::middleware(ValidUser::class)->group(function(){
+
+//     Route::get('/add-customer', [AddCustomerController::class, 'showForm'])->name('add.customer.form');
+//     Route::get('/dashboard', [LoginController::class, 'showDashboard'])->name('dashboard');
+//     Route::get('/customer/{id}/edit', [UpdateCustomerController::class, 'edit'])->name('edit.customer');
+// });
